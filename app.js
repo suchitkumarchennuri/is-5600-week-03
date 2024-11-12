1794d38d5d568cefa565c32c2ffe4517ad2c00bb
suchitkumarchennuri <143919454+suchitkumarchennuri@users.noreply.github.com>
2024-11-12 19:08:32 +0000

    added respondText, RespondJSON, 404 Not found error page.

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ const chatEmitter = new EventEmitter();
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
+ * Responds with plain text
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+/**
+ * Responds with JSON
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondJson(req, res) {
+  res.json({
+    text: 'hi',
+    numbers: [1, 2, 3],
+  });
+}
+
+/**
+ * Responds with a 404 not found
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondNotFound(req, res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+}
+
+/**
  * Serves up the chat.html file
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
@@ -18,11 +50,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'chat.html'));
 });
 
-/**
- * This endpoint will receive chat messages and emit them to all listeners
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- */
+// Route to respond with plain text
+app.get('/text', respondText);
+
+// Route to respond with JSON
+app.get('/json', respondJson);
+
+// Endpoint to receive chat messages and emit them to all listeners
 app.get('/chat', (req, res) => {
   const { message } = req.query;
   if (message) {
@@ -32,7 +66,7 @@ app.get('/chat', (req, res) => {
 });
 
 /**
- * This endpoint will respond to the client with a stream of server-sent events
+ * SSE endpoint to respond to the client with a stream of server-sent events
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  */
@@ -49,6 +83,9 @@ app.get('/sse', (req, res) => {
     chatEmitter.off('message', onMessage);
   });
 });
+
+// Handle 404 for undefined routes
+app.use(respondNotFound);
 
 // Start server
 app.listen(port, () => {
